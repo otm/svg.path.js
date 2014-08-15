09560572a0bf59b451e99e2025240ad4108f28d3
Nils Lagerkvist <nils.lagerkvist@tele2.com>
2014-08-15 13:47:45 +0000

    Added functionality to update svg.js via grunt update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,14 @@
 module.exports = function(grunt) {
-
 	// Project configuration.
 	grunt.initConfig({
 		// Metadata.
 		bwr: grunt.file.readJSON('bower.json'),
 		pkg: grunt.file.readJSON('package.json'),
-		custom: {
-			remove: function(str) {
-				return String(str).remove(/\.js$/, "");
-			}
-		},
 		banner: '/** <%= pkg.main || pkg.name %>.js - v<%= pkg.version %> - ' +
 			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
 			'<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
 			' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-			' Licensed under the  <%= pkg.license %> license /\n */\n',
+			' Licensed under the <%= pkg.license %> license */\n',
 		// Task configuration.
 		concat: {
 			options: {
@@ -59,6 +53,17 @@ module.exports = function(grunt) {
 				files: '<%= jshint.svgpathfile.src %>',
 				tasks: ['jshint:svgpathfile']
 			}
+		},
+		bower: {
+			install: {
+				options: {
+					targetDir: "spec/lib/",
+					cleanup: true,
+					bowerOptions: {
+						production: true
+					}
+				}
+			}
 		}
 	});
 
@@ -67,8 +72,31 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-bower-task');
+
+	/* check if we remembered to sync versions in bower.json and package.json,
+	 * the version from package.json is written in the banner */
+	grunt.registerTask('version', 'Test bower.json and package.json versions', function version() {
+		grunt.log.write('Checking if versions are in sync...');
+		grunt.config.requires('bwr.version');
+		grunt.config.requires('pkg.version');
+		if( grunt.config.data.bwr.version === grunt.config.data.pkg.version ) {
+			grunt.log.ok();
+			return true;
+		} else {
+			grunt.log.error(
+				'bower.json version '
+			 + grunt.config.data.bwr.version
+			 + ' is not in sync with package.json version '
+			 + grunt.config.data.pkg.version
+			 + '.'
+			);
+			return false;
+		}
+	});
 
 	// Default task.
-	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
-
+	grunt.registerTask('default', ['jshint', 'version', 'concat', 'uglify']);
+	// update svg.js using bower and reading config from bower.json
+	grunt.registerTask('update', ['bower']);
 };
--- svg.path.js
+++ svg.path.js
@@ -1,7 +1,6 @@
 /** svg.path.js - v0.6.0 - 2014-08-15
  * http://otm.github.io/svg.path.js/
- * Copyright (c) 2014 Nils Lagerkvist; Licensed under the  MIT license /
- */
+ * Copyright (c) 2014 Nils Lagerkvist; Licensed under the MIT license */
 (function() {
 
 	var slice = Function.prototype.call.bind(Array.prototype.slice);
